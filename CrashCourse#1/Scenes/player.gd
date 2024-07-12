d9dc1e72f5d2857244a5aa2fccef23904718e666
Gontzal Aparicio <gaparicio@centum.es>
2024-07-12 13:02:49 +0200

    [GodotCrashCourse]: Coding GDScript basics

--- CrashCourse#1/Scenes/player.gd
+++ CrashCourse#1/Scenes/player.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
+var player_node = ""
+var speed: Vector2 = Vector2(0, 0)
+# The ':=' sets de type of the base_speed to the type of the value assigned
+@export var base_speed := 300
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player_node = $PlayerImage
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var direction = Input.get_vector("left", "right", "up", "down")
+	speed = direction * base_speed
+	#position += Vector2(1, 0) * 100 * delta
+	player_node.position += speed * delta
+	speed = Vector2(0, 0)
+
