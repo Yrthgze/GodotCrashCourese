9747ce281f6081e55ea4c4761afc0906235e8212
Gontzal Aparicio <gaparicio@centum.es>
2024-07-15 16:18:53 +0200

    [GodotCrashCourse]: Physics colliders 2D and signals.

--- CrashCourse#1/Scenes/player.gd
+++ CrashCourse#1/Scenes/player.gd
@@ -1,9 +1,8 @@
-extends Node2D
+extends CharacterBody2D
 
 var player_node = ""
-var speed: Vector2 = Vector2(0, 0)
 # The ':=' sets de type of the base_speed to the type of the value assigned
-@export var base_speed := 300
+@export var base_speed := 500
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player_node = $PlayerImage
@@ -11,8 +10,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	speed = direction * base_speed
-	#position += Vector2(1, 0) * 100 * delta
-	player_node.position += speed * delta
-	speed = Vector2(0, 0)
+	velocity = direction * base_speed
+	move_and_slide()
 
