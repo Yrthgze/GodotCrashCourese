247b8f48d52f220b4ed409c623562a9331e477bc
Gontzal Aparicio <gaparicio@centum.es>
2024-07-16 09:28:55 +0200

    [BugMeteorInstanceXOffset]: The meteor container scene had an offset. Reset it.

--- CrashCourse#1/Scenes/meteor.gd
+++ CrashCourse#1/Scenes/meteor.gd
@@ -5,10 +5,10 @@ func _ready():
 	var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 	var width = get_viewport().get_visible_rect().size[0]
 	
-	var random_x = 0# rng.randi_range(30, width-30)
-	print(random_x)
+	var random_x = rng.randi_range(0, width)
 	var random_y = rng.randi_range(-150, -50)
 	position = Vector2(random_x, random_y)
+	print(position)
 
 func _process(delta):
 	position += Vector2(0, 1) * 400 * delta
