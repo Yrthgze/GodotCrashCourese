b19fea5829ac1db7be05808cc492567f3c3cd4c0
Gontzal Aparicio <gontzal.aparicio@centum.com>
2024-07-16 15:35:43 +0200

    [GodotCrashCourse]: Fire limiting

--- CrashCourse#1/Scenes/level.gd
+++ CrashCourse#1/Scenes/level.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
 var meteor_scene: PackedScene = load("res://Scenes/meteor.tscn")
+var laser_scene: PackedScene = load("res://Scenes/laser.tscn")
 var meteor_list = ""
+var laser_list = ""
+var laser_ready = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	meteor_list = $Meteors
+	laser_list = $Lasers
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -13,3 +17,8 @@ func _process(delta):
 func _on_meteor_timer_timeout():
 	var new_meteor = meteor_scene.instantiate()
 	meteor_list.add_child(new_meteor)
+
+func _on_player_laser(pos):
+	var new_laser = laser_scene.instantiate()
+	laser_list.add_child(new_laser)
+	new_laser.position = pos
--- CrashCourse#1/Scenes/player.gd
+++ CrashCourse#1/Scenes/player.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
-
+signal Laser(pos)
 var player_node = ""
 # The ':=' sets de type of the base_speed to the type of the value assigned
 @export var base_speed := 500
+var laser_ready = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player_node = $PlayerImage
 
+func shoot():
+	Laser.emit($LaserStartPos.global_position)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * base_speed
 	move_and_slide()
-
+	if Input.is_action_just_pressed("shoot") and laser_ready:
+		shoot()
+		laser_ready = false
+		$LaserTimer.start()
+		
+func _on_laser_timer_timeout():
+	laser_ready = true
