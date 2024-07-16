e373aec2f24507552d9f756e175d40b8c9194be9
Gontzal Aparicio <gontzal.aparicio@centum.com>
2024-07-16 12:53:07 +0200

    [GodotCrashCourse]: Layer and mask cleaning. Collisions ordering

--- CrashCourse#1/Scenes/meteor.gd
+++ CrashCourse#1/Scenes/meteor.gd
@@ -2,6 +2,7 @@ extends Area2D
 var meteorColors = ["Brown", "Grey"]
 var meterSizes = ["big", "med", "small", "tiny"]
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var speed = rng.randi_range(10, 400)
 var rotation_speed = rng.randf_range(-0.1, 0.1)
 var direction_change = rng.randf_range(-0.5, 0.5)
 
@@ -11,7 +12,8 @@ func select_random_sprite():
 	var meteorSize = meterSizes[rng.randi_range(0, meterSizes.size() - 1)]
 	var sprite_number = 1;
 	var final_path = base_path + meteorColor + "_" + meteorSize + str(sprite_number) + ".png"
-	$Sprite2D.texture = ResourceLoader.load(final_path)
+	$Sprite2D.texture = load(final_path)
+	
 func _ready():
 	select_random_sprite()	
 	
@@ -21,8 +23,9 @@ func _ready():
 	position = Vector2(random_x, random_y)
 
 func _process(delta):
-	position += Vector2(direction_change, 1) * 400 * delta
+	position += Vector2(direction_change, 1) * speed * delta
 	rotation += rotation_speed
 	
 func _on_body_entered(body):
-	pass
+	print("Collide with ")
+	print(body)
